d31074206b06b8cc2ecadf80530b1841be032cc3
Filippo Tessaro <filippo.tessaro@studenti.unitn.it>
2018-01-03 14:26:46 +0100

    effettuato primo commit

---
+++ index.js
@@ -0,0 +1,57 @@
+/*globals require, console, process */
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// instantiate express
+const app = express();
+
+
+
+app.use(express.static(__dirname + '/public')); 				// set the static files location /public/img will be /img for users
+
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// set our port
+var port = process.env.PORT || 8080;
+
+
+app.get('/', function(req, res) {
+    res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
+
+app.use(function (req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    //Enabling CORS
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Content-Type', 'application/json');
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE');
+        return res.status(200).json({});
+    }
+    // make sure we go to the next routes
+    next();
+});
+
+
+// handle invalid requests and internal error
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({ error: { message: err.message } });
+});
+
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
