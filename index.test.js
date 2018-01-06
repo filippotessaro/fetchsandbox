4822a98b49f8d0fc2c9189ed929bf8a9b30061cd
Filippo Tessaro <filippo.tessaro@studenti.unitn.it>
2018-01-06 16:23:05 +0100

    aggiunte altre funzioni di test

--- index.test.js
+++ index.test.js
@@ -18,11 +18,47 @@ const postNote = function(note){
     })
 }
 
+const getOneNote = function(noteID){
+    return fetch('https://fetchsandbox.herokuapp.com/api/notes/'+noteID, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+}
+
+const deleteNote = function(noteID){
+    return fetch('https://fetchsandbox.herokuapp.com/api/notes/'+noteID, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }//,
+    })
+}
+
 test('basic post and get the posted element', () =>{
     return postNote(exampleNote)
         .then(res => {return res.json()})
         .then(data =>{
-            expect(data.body).toBe(exampleNote.body);
-            expect(data.title).toBe(exampleNote.title);
+            exampleNote.noteID = data._id
+            return getOneNote(exampleNote.noteID)
+        })
+        .then(getResponse => {return getResponse.json()})
+        .then(jsonresponse => {
+            expect(jsonresponse.body).toBe(exampleNote.body);
+            expect(jsonresponse.title).toBe(exampleNote.title);
+            expect(jsonresponse._id).toBe(exampleNote.noteID);
         })
 });
+
+test('delete sample item', () =>{
+    return deleteNote(exampleNote.noteID)
+    .then(res => {return res.json()})    
+    .then(response => {expect(response.message).toBe('Succesfully deleted');})
+});
+
+// test('delete item actually deteted', ()=>{
+//     return deleteNote(exampleNote.noteID)
+//         .then(response => {expect(response.status).toBe(404);})
+// });
